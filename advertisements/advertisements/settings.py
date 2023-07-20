76d35851debd252d485704d5ddff09c86f0e0903
Danchick07 <123@mail.ru>
2023-07-20 10:43:34 +0300

    добавлена БД

--- advertisements/advertisements/settings.py
+++ advertisements/advertisements/settings.py
@@ -78,6 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'postgeSQL': {
+        'ENGINE': 'django.db.backends.postgeSQL',
+        'NAME': BASE_DIR / 'postgeSQL',
     }
 }
 
