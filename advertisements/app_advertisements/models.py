dee6da7335a74ba70e83712569938d2b5b847d54
Danchick07 <123@mail.ru>
2023-08-09 12:41:50 +0300

    добавлены объявления

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib import admin
-
+from django.utils.html import format_html
+from django.contrib.auth import get_user_model
 class Advertisements(models.Model):
     title = models.CharField("Заголовок", max_length=128)
     description = models.TextField('Описание')
@@ -14,29 +15,30 @@ class Advertisements(models.Model):
     @admin.display(description='Дата создания')
     def created_date(self):
         from django.utils import timezone
-        from django.utils.html import format_html
         if self.created_at.date() == timezone.now().date():
-            created_time = self.created_at.time().strftime('%H:%M:%S')
+            created_time = self.created_at.time().strftime("%H:%M:%S")
             return format_html(
-                '<span style="color: green, font_weight: bold">'
-                'Сегодня в {}</span>', created_time
+                '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time
             )
-        return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
 
-    @admin.display(description='Дата обновления')
+    @admin.display(description='Дата последнего обновления')
     def update_date(self):
         from django.utils import timezone
-        from django.utils.html import format_html
         if self.update_at.date() == timezone.now().date():
             update_time = self.update_at.time().strftime('%H:%M:%S')
             return format_html(
-                '<span style="color: red, font_weight: lighter">'
-                'Сегодня в {}</span>', update_time
+                '<span style="color: blue; font-weight: bold;">Сегодня в {}</span>', update_time
             )
         return self.update_at.strftime('%d.%m.%Y в %H:%M:%S')
+
 
 class Meta:
     db_table = "advertisements"
 
-
 # Create your models here.
+
+
+
+
+
